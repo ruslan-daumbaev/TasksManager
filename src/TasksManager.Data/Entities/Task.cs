8c4480e2e3e9a36a29a08b2dc6069f66551e0e47
Ruslan Ddaumbaev <ruslan.daumbaev@gmail.com>
2018-12-28 23:43:18 +0300

    Added Data project

---
+++ src/TasksManager.Data/Entities/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TasksManager.Data.Entities
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public string Priority { get; set; }
+
+        public DateTime TimeToComplete { get; set; }
+
+        public DateTime AddedDate { get; set; }
+
+    }
+}
